1ae02a3c6b87f931184269a07a818daa12ca699c
tkmy <b1704437@planet.kanazawa-it.ac.jp>
2019-07-22 08:30:25 +0900

    fixed gpsrNode.py Navigation.py

--- src/gpsrNode.py
+++ src/gpsrNode.py
@@ -13,37 +13,41 @@ class GPSRNode:
         self.navi_result_sub = rospy.Subscriber('/navigation/result',String,self.navigateResult)
         self.mani_result_sub = rospy.Subscriber('/object/grasp_res',Bool,self.manipulateResult)
         self.search_result_sub = rospy.Subscriber('/object/recog_res',Bool,self.searchResult)
-        self.arm_change_result_sub = rospy.Subscriber('/arm_change/result',String,self.arm_changeResult)#未使用
         
         self.destination_pub = rospy.Publisher('/navigation/destination',String,queue_size=1)
         self.search_pub = rospy.Publisher('/object/recog_req',String,queue_size=10)
         self.manipulation_pub = rospy.Publisher('/object/grasp_req',String,queue_size=10)
-        self.changing_pose_req_pub = rospy.Publisher('/arm/changing_pose_req',String,queue_size=1)#未使用
+        self.changing_pose_req_pub = rospy.Publisher('/arm/changing_pose_req',String,queue_size=1)
         self.m6_reqest_pub = rospy.Publisher('/m6_controller/command',Float64,queue_size=1)
-        self.API_pub = rospy.Publisher('/face',Bool,queue_size=10)#未使用
-        self.API_pub = rospy.Publisher('/face',Bool,queue_size=10)#未使用
+        self.gpsrAPI_pub = rospy.Publisher('/gpsrface',Bool,queue_size=10)
 
+        #最低限必要な変数
         self.sub_state = 0
-        self.command = 'command'
+        self.API_state = 0
+        self.task_count = 0
+        self.action = 'none'
+        self.location = 'none'
+        self.obj = 'none'
+        self.answer = 'none'
+        #タスクをするために必要な動作
         self.navigate = 'go'
         self.search_object = 'find'
         self.manipulate_object = 'manipulate'
         self.return_operator = 'return'
         self.finish = 'finish'
-        self.task_count = 0
-        self.answer_result = 'Null'
-        self.navigation_result = 'Null'
-        self.search_result = 'Null'
-        self.manipulation_result = 'Null'
-        self.arm_change_result = 'Null'
-
-        self.action = 'none'
-        self.location = 'none'
-        self.obj = 'none'
-        self.answer = 'none'
-
+        #各result
+        self.navigation_result = 'null'
+        self.search_result = False
+        self.manipulation_result = False
+        #実行可能な動作リスト
+        self.com_list = ['Tell','Locate','Find','Bring','Take']
+       
     def Command(self,command_list):
-        self.action = command_list.action
+        print 'action:{action} location:{location} obj:{obj} answer:{answer}'.format(action=command_list.action,location=command_list.location,obj=command_list.obj,answer=command_list.answer)#test
+        rospy.sleep(3)#test
+        for num in range(len(self.com_list)):
+            if self.com_list[num] in command_list.action:
+                self.action = self.com_list[num]#command_list.action
         self.location = command_list.location
         self.obj = command_list.obj
         self.answer = command_list.answer
@@ -56,15 +60,18 @@ class GPSRNode:
             print 'navi'
             if self.navigation_result == 'succsess':
                 self.search_pub.publish(self.obj)
+                self.navigation_result = 'null'
                 self.sub_state = 2
         elif self.sub_state == 2:
             print 'search'
             if self.search_result == True:
                 self.destination_pub.publish("operator")
+                self.search_result = False
                 self.sub_state = 3
         elif self.sub_state == 3:
             print 'return'
-            if self.navigation_result == 'return_op':
+            if self.navigation_result == 'succsess':
+                self.navigation_result = 'null'
                 self.sub_state = 0
                 self.action = 'none'
                 self.location = 'none'
@@ -80,15 +87,18 @@ class GPSRNode:
             print 'navi'
             if self.navigation_result == 'succsess':
                 rospy.sleep(2)
-                CMD = '/usr/bin/picospeaker {answer}'.format(answer=self.answer)
+                CMD ='/usr/bin/picospeaker {answer}'.format(answer=self.answer)
+                subprocess.call(CMD.strip().split(" "))
                 rospy.sleep(1)#sentenceの長さによって変更
+                self.navigation_result = 'null'
                 self.sub_state = 2
         elif self.sub_state == 2:
              self.destination_pub.publish("operator")
              self.sub_state = 3
         elif self.sub_state == 3:
             print 'return'
-            if self.navigation_result == 'return_op':
+            if self.navigation_result == 'succsess':
+                self.navigation_result = 'null'
                 self.sub_state = 0
                 self.action = 'none'
                 self.location = 'none'
@@ -104,15 +114,18 @@ class GPSRNode:
             print 'navi'
             if self.navigation_result == 'succsess':
                 self.search_pub.publish(self.obj)
+                self.navigation_result = 'null'
                 self.sub_state = 2
         elif self.sub_state == 2:
             print 'search'
             if self.search_result == True:
                 self.destination_pub.publish("operator")
+                self.search_result = False
                 self.sub_state = 3
         elif self.sub_state == 3:
             print 'return'
-            if self.navigation_result == 'return_op':
+            if self.navigation_result == 'succsess':
+                self.navigation_result = 'null'
                 self.sub_state = 0
                 self.action = 'none'
                 self.location = 'none'
@@ -128,22 +141,44 @@ class GPSRNode:
             print 'navi'
             if self.navigation_result == 'succsess':
                 self.manipulation_pub.publish(self.obj)
+                self.navigation_result = 'null'
                 self.sub_state =2
         elif self.sub_state == 2:
             print 'mani'
             if self.manipulation_result == True:
-                self.destination_pub.publish("operator")
+                print 'state:arm change'
+                self.changing_pose_req_pub.publish('carry')
+                rospy.sleep(3)#かかる時間によって変更
                 self.sub_state = 3
-        elif self.sub_state == 3:
+                self.manipulation_result = False
+        elif self.sub_state == 3: 
+                self.destination_pub.publish("operator")
+                self.sub_state = 4
+        elif self.sub_state == 4:
             print 'return'
-            if self.navigation_result == 'return_op':
-                self.sub_state = 0
-                self.action = 'none'
-                self.location = 'none'
-                self.obj = 'none'
-                self.answer = 'none'
-                self.task_count+=1
+            if self.navigation_result == 'succsess':
+                self.navigation_result = 'null'
+                self.changing_pose_req_pub.publish('pass')#人にオブジェクトを渡すためにアームの角度を変える必要があれば。
+                rospy.sleep(3)#かかる時間によって変更
+                self.sub_state = 5
+        elif self.sub_state == 5:
+            CMD = '/usr/bin/picospeaker %s' % 'Here you are'
+            subprocess.call(CMD.strip().split(" "))
+            rospy.sleep(2)#時間の調整あり
+            self.sub_state = 6
+        elif self.sub_state == 6:
+            CMD = '/usr/bin/picospeaker %s' % 'You are welcome'
+            subprocess.call(CMD.strip().split(" "))
+            rospy.sleep(1.5)#時間の調整あり 
+            self.sub_state = 0
+            self.action = 'none'
+            self.location = 'none'
+            self.obj = 'none'
+            self.answer = 'none'
+            self.task_count+=1
                 
+    #基本bringと同じ処理。把持したものをオペレーター以外の場所に持って行くなら違う処理になるので、
+    #把持した後にnaviして、naviした場所のどこかに物体を置いてオペレーターに戻って終了
     def take(self):
         if self.sub_state == 0:
             self.destination_pub.publish(self.location)
@@ -152,35 +187,63 @@ class GPSRNode:
             print 'navi'
             if self.navigation_result == 'succsess':
                 self.manipulation_pub.publish(self.obj)
-                self.sub_state = 2
+                self.navigation_result = 'null'
+                self.sub_state =2
         elif self.sub_state == 2:
             print 'mani'
             if self.manipulation_result == True:
-                self.destination_pub.publish("operator")
+                print 'state:arm change'
+                self.changing_pose_req_pub.publish('carry')
+                rospy.sleep(3)#かかる時間によって変更
                 self.sub_state = 3
-        elif self.sub_state == 3:
+                self.manipulation_result = False
+        elif self.sub_state == 3: 
+                self.destination_pub.publish("arg")#変更必須!argは物体を置く場所
+                self.sub_state = 4
+        elif self.sub_state == 4:
+            print 'navi'
+            if self.navigation_result == 'succsess':
+                self.navigation_result = 'null'
+                self.changing_pose_req_pub.publish('put')#ある場所のテーブルに物体を置く。もしかしたら時間でなく、成功したか判断しないといけないかも。
+                rospy.sleep(60)#かかる時間によって変更または、実行結果を受け取る必要がある
+                self.sub_state = 5
+        elif self.sub_state == 5:
+            self.destination_pub.publish("operator")
+            self.sub_state = 6
+        elif self.sub_state == 6:
             print 'return'
-            if self.navigation_result == 'return_op':
+            if self.navigation_result == 'succsess':
+                self.navigation_result = 'null'
                 self.sub_state = 0
                 self.action = 'none'
                 self.location = 'none'
                 self.obj = 'none'
                 self.answer = 'none'
                 self.task_count+=1
-    #それぞれの関数の最後にgpsr.return_opratet()をつける
+                
+    """def sample(self):#新たな命令を追加する場合はこれを参考にして下さい
+        self.gpsrAPI_pub.publish(False)
+        self.com = self.action
+        self.API_state = 0
+        #ここに処理(navi,mani..)を書く
+        self.sub_state = 0
+        self.com = 'null'
+        self.action = 'none'
+        self.location = 'none'
+        self.obj = 'none'
+        self.answer = 'none'
+        self.task_count+=1"""
 
     def finishState(self):
-        #self.API_pub.publish('False')
-        self.action = 'fin'
+        self.gpsrAPI_pub.publish(False)
+        self.action = 'finish'
         self.destination_pub.publish('entrance')
         print 'navi entrance'
         if self.navigation_result == 'succsess':
-            print 'おわり。'
-            rospy.sleep(1)
-            CMD = '/usr/bin/picospeaker %s' % 'I finished gpsr'
+            CMD = '/usr/bin/picospeaker %s' % 'Finished gpsr'
             subprocess.call(CMD.strip().split(" "))
             print ''
-            print "I finished GPSR"
+            print 'finish GPSR'
             exit()
             
     def navigateResult(self,result):
@@ -192,9 +255,6 @@ class GPSRNode:
     def manipulateResult(self,result):
         self.manipulation_result = result.data
 
-    def arm_changeResult(self,result):
-        self.arm_change_result = result.data
-
     def loopMain(self):
         print '///start GPSR//'
         while not rospy.is_shutdown():
@@ -203,18 +263,25 @@ class GPSRNode:
                 print '--{action}-- [task_count:{count}]'.format(action=self.action,count=self.task_count)
                 if self.task_count == 3:#task_count == 繰り返したい回数
                     self.finishState()    
-                if self.action == "Tell":
-                    self.tell()
-                elif self.action == "Locate":
-                    self.locate()
-                elif self.action == "Find":
-                    self.find()
-                elif self.action == "Bring":
-                    self.bring()
-                elif self.action == "Take":
-                    self.take()
-                elif self.action == "none":
-                    print "command waiting.."
+                if self.action == 'none':
+                    if self.API_state == 0:
+                        self.gpsrAPI_pub.publish(True)
+                        self.API_state = 1
+                    elif self.API_state == 1:
+                        print "command waiting.."
+                elif self.action != 'none':
+                    self.gpsrAPI_pub.publish(False)
+                    self.API_state = 0  
+                    if self.action == "Tell":
+                        self.tell()
+                    elif self.action == "Locate":
+                        self.locate()
+                    elif self.action == "Find":
+                        self.find()
+                    elif self.action == "Bring":
+                        self.bring()
+                    elif self.action == "Take":
+                        self.take()              
             except IndexError:
                 pass
             rospy.sleep(0.5)    
@@ -222,5 +289,6 @@ class GPSRNode:
 if __name__ == '__main__':
     rospy.init_node('gpsr_node')
     gpsr = GPSRNode()
+    rospy.sleep(1)
     gpsr.loopMain()
     rospy.spin()
